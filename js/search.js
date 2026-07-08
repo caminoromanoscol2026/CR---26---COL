6aab90fe68dede432a31da09f9205ffb64656890
Manu <caminoromanoscol2026@gmail.com>
2026-07-08 00:55:14 -0500

    v04

--- js/search.js
+++ js/search.js
@@ -17,38 +17,19 @@ function buscarParticipantes(e) {
     const resultados = document.getElementById("results");
 
     if (texto === "") {
-
         resultados.innerHTML = "";
         return;
-
     }
 
     const encontrados = state.participantes.filter(p => {
 
         return (
-
             (p["Nombre completo"] || "").toLowerCase().includes(texto) ||
-
-            String(p["N° de cedula o pasaporte"] || "")
-                .toLowerCase()
-                .includes(texto) ||
-
-            (p["Correo electrónico"] || "")
-                .toLowerCase()
-                .includes(texto) ||
-
-            (p["Ciudad - Departamento"] || "")
-                .toLowerCase()
-                .includes(texto) ||
-
-            (p["País"] || "")
-                .toLowerCase()
-                .includes(texto) ||
-
-            (p["Iglesia o ministerio al que pertenece"] || "")
-                .toLowerCase()
-                .includes(texto);
-
+            String(p["N° de cedula o pasaporte"] || "").toLowerCase().includes(texto) ||
+            (p["Correo electrónico"] || "").toLowerCase().includes(texto) ||
+            (p["Ciudad - Departamento"] || "").toLowerCase().includes(texto) ||
+            (p["País"] || "").toLowerCase().includes(texto) ||
+            (p["Iglesia o ministerio al que pertenece"] || "").toLowerCase().includes(texto)
         );
 
     });
